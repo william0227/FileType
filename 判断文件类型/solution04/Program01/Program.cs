4dac0418d7480d7f1b8cb435e2bfa7978e40c1a5
william0227 <willia@sina.cn>
2015-09-10 14:34:37 +0800

    the 2nd edition
    判断文件类型

--- 判断文件类型/solution04/Program01/Program.cs
+++ 判断文件类型/solution04/Program01/Program.cs
@@ -5,19 +5,18 @@ using System.Text;
 using System.Threading.Tasks;
 using System.IO;
 
-namespace Program01
+
+namespace FileType
 {
     //声明一个文件类————基类
     class FileClass
     {
         private string name;        //文件名成员
 
-
         //基类的构造函数
         public FileClass(string Name)
         {
             name = Name;
-
         }
 
         //判断类型虚方法，在基类中不实现
@@ -25,94 +24,26 @@ namespace Program01
         {
             return false;
         }
-
-
-    }
-
-    //只有header或者只有trailer的子类，继承FileClass
-    class HeaderOnly : FileClass
-    {
-
-        private string fileString;                   //接收header或者trailer的字符串
-        private string header;                          //header 或trailer
-        private int offset;                         //偏移量
-        private int byteNum;                       //需读取的字节数
-
-
-        //HeaderOnly的构造函数
-        public HeaderOnly(string name, string Header, int Offset, int ByteNum)
-            : base(name)
-        {
-            fileString = "";
-            header = Header;
-            offset = Offset;
-            byteNum = ByteNum;
-
-
-            FileStream fs;                                              //文件流
-            fs = new FileStream(name, FileMode.Open, FileAccess.Read); //从文件名读取一个文件流
-            string FileString = "";
-            fs.Seek(offset, SeekOrigin.Begin);              //设置文件开始读取的位置
-            BinaryReader reader = new BinaryReader(fs);     //初始化二进制文件读写器，读取文件流
-            byte[] b = new byte[byteNum];                   //接收文件内容的byte数组
-
-            //异常处理
-            try
-            {
-                for (int i = 0; i < b.Length; i++)
-                {
-                    b[i] = reader.ReadByte();                   //将文件内容读入数组
-                }
-            }
-            catch (Exception)
-            {
-                throw;
-            }
-
-            fs.Close();                             //关闭文件流，释放资源
-            reader.Dispose();                       //释放二进制读取器的资源
-
-            for (int i = 0; i < b.Length; i++)
-            {
-                FileString += b[i].ToString("X2");        //将字节数组内容转为字符串(16进制)
-            }
-
-            fileString = FileString;
-        }
-
-
-        //判断是否为该类型,重写基类中的虚方法
-        public override bool IsThisType()
-        {
-            if (fileString.IndexOf(header) != -1)
-            {
-                return true;
-            }
-            else
-            {
-                return false;
-            }
-        }
-
     }
 
 
-    //既有header也有trailer的子类，继承FileClass
-    class HeaderAndTrailer : FileClass
+    //NewFile子类，继承FileClass
+    class NewFile : FileClass
     {
-
         private string fileString_header;                   //接收header的字符串
         private string fileString_trailer;                   //接收trailer的字符串
-        private string header;                          //header
-        private string trailer;                         //trailer
+        private string header;                              //header
+        private string trailer;                             //trailer
         private int offset_header;                         //header的偏移量
-        private int offset_trailer;                     //trailer的偏移量
-        private int byteNum_header;                       //header需读取的字节数
-        private int byteNum_trailer;                       //trailer需读取的字节数
-
+        private int offset_trailer;                         //trailer的偏移量
+        private int byteNum_header;                         //header需读取的字节数
+        private int byteNum_trailer;                        //trailer需读取的字节数
 
         //HeaderOnly的构造函数
-        public HeaderAndTrailer(string name, string Header, string Trailer, int Offset_Header, int Offset_Trailer, int ByteNum_Header, int ByteNum_Trailer)
+        //若只有header 则构造时令trailer = "0" , byteNum_trailer = 0；
+        //否则，正常构造
+
+        public NewFile(string name, string Header, string Trailer, int Offset_Header, int Offset_Trailer, int ByteNum_Header, int ByteNum_Trailer)
             : base(name)
         {
             fileString_header = "";
@@ -126,19 +57,19 @@ namespace Program01
 
 
             //先读header
-            FileStream fs;      
-            fs = new FileStream(name, FileMode.Open, FileAccess.Read); 
+            FileStream fs;
+            fs = new FileStream(name, FileMode.Open, FileAccess.Read);
             string FileString = "";
             fs.Seek(offset_header, SeekOrigin.Begin);             //header从头读
-            BinaryReader reader1 = new BinaryReader(fs);     
-            byte[] b1 = new byte[byteNum_header];                  
+            BinaryReader reader1 = new BinaryReader(fs);
+            byte[] b1 = new byte[byteNum_header];
 
             //异常处理
             try
             {
                 for (int i = 0; i < b1.Length; i++)
                 {
-                    b1[i] = reader1.ReadByte();                  
+                    b1[i] = reader1.ReadByte();
                 }
             }
             catch (Exception)
@@ -146,53 +77,58 @@ namespace Program01
                 throw;
             }
 
-            
-           
 
             for (int i = 0; i < b1.Length; i++)
             {
-                FileString += b1[i].ToString("X2");        
+                FileString += b1[i].ToString("X2");
             }
 
             fileString_header = FileString;         //获取header
 
 
+            BinaryReader reader2 = new BinaryReader(fs);
 
 
-            //再读trailer
-            FileString = "";
-            fs.Seek(offset_trailer, SeekOrigin.End);              //trailer从尾读
-            BinaryReader reader2 = new BinaryReader(fs);     
-            byte[] b2 = new byte[byteNum_trailer];                   
-
-            //异常处理
-            try
+            //若无trailer，则直接匹配
+            if (byteNum_trailer == 0)
             {
+                fileString_trailer = trailer;
+            }
+
+            //若有trailer，则接着读trailer
+            else
+            {
+                FileString = "";
+                fs.Seek(offset_trailer, SeekOrigin.End);              //trailer从尾读
+                byte[] b2 = new byte[byteNum_trailer];
+
+                //异常处理
+                try
+                {
+                    for (int i = 0; i < b2.Length; i++)
+                    {
+                        b2[i] = reader2.ReadByte();
+                    }
+                }
+                catch (Exception)
+                {
+                    throw;
+                }
+
                 for (int i = 0; i < b2.Length; i++)
                 {
-                    b2[i] = reader2.ReadByte();                   
+                    FileString += b2[i].ToString("X2");
                 }
-            }
-            catch (Exception)
-            {
-                throw;
+
+                fileString_trailer = FileString;        //获取trailer
             }
 
-            reader1.Dispose();                       
+            reader1.Dispose();
             reader2.Dispose();                       //释放二进制读取器的资源
             fs.Close();                             //关闭文件流
-
-            for (int i = 0; i < b2.Length; i++)
-            {
-                FileString += b2[i].ToString("X2");       
-            }
-
-            fileString_trailer = FileString;        //获取trailer
         }
 
-
-
-        //判断是否为该类型,重写基类中的虚方法
+        //重写基类中的虚方法
         public override bool IsThisType()
         {
             //同时匹配  header 和 trailer
@@ -209,23 +145,22 @@ namespace Program01
     }
 
 
+
     class Program
     {
-
         //判断文件类型的函数
-        static void FileType(string name)
+        static void DetectFileType(string name)
         {
-            string Name = name;
             int Flag = 0;       //初始化标志
 
-            FileClass newZip = new HeaderOnly(Name, "504B0304", 0, 4);      //生成zip类型的对象
-            
+            FileClass newZip = new NewFile(name, "504B0304", "0", 0, 0, 4, 0);      //生成zip类型的对象
+
             if (newZip.IsThisType())
             {
                 Flag = 1;       //zip标号为1
             }
 
-            FileClass newGif = new HeaderAndTrailer(Name, "474946383961", "003B", 0, -2, 6, 2); //生成gif类型的对象
+            FileClass newGif = new NewFile(name, "474946383961", "003B", 0, -2, 6, 2); //生成gif类型的对象
             if (newGif.IsThisType())
             {
                 Flag = 3;   //gif标号为3
@@ -238,21 +173,16 @@ namespace Program01
                 default: Console.WriteLine("文件类型未知"); break;
             }
         }
-
-
-        //主函数，用户只需输入文件名
         static void Main(string[] args)
         {
-            string Name = "";
+            string fileName = "";
 
-            Console.WriteLine("请输入文件名");
-            Name = Console.ReadLine();
+            Console.WriteLine("请要判断类型的输入文件名：");
+            fileName = Console.ReadLine();
 
-            FileType(Name);
-
+            DetectFileType(fileName);
 
             Console.ReadKey();
         }
     }
-
 }
